17a86215a20dad898fda2824b7e15262a7047e2b
Shammi141 <jannatulshammi181@gmail.com>
2022-09-14 23:00:19 +0600

    added blog file

--- script.js
+++ script.js
@@ -68,7 +68,6 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  //const timeTaken = (finishTime - startTime) / 1000;
   const timeTaken = Math.round((finishTime - startTime) / 1000);
 
   // show result modal
@@ -109,14 +108,12 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    //countdownOverlay.innerHTML = '<h1> ${count} </h1>';
     countdownOverlay.innerHTML = `<h1> ${count} </h1>`;
 
     // finished timer
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      //countdownOverlay.style.display = "flex";
       countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
